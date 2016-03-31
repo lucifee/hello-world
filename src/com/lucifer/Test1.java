57d69277f053fff399053022bf3ece7efa9111a3
lucifee <lcfseed@163.com>
2016-03-31 15:54:41 +0800

    commit1

---
+++ src/com/lucifer/Test1.java
@@ -0,0 +1,18 @@
+package com.lucifer;
+
+/**
+ * @author Lucifer
+ *
+ * 2016年3月31日 下午3:52:31
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
